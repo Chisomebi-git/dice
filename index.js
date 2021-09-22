fee251dd1467002b19a5eaea69590e8ab9b18880
Chisomebi Onwunyi <68714243+Chisomebi-git@users.noreply.github.com>
2021-09-22 12:10:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-var player1 = Math.floor(1 + Math.random() * 6);
-var player2 = Math.floor(1 + Math.random() * 6);
+document.querySelector("button").addEventListener("click", diceRoll);
+function diceRoll(){
+    var player1 = Math.floor(1 + Math.random() * 6);
+    var player2 = Math.floor(1 + Math.random() * 6);
 
-var image1 = "images/dice" + player1 + ".png";
-var image2 = "images/dice" + player2 + ".png"; 
+    var image1 = "images/dice" + player1 + ".png";
+    var image2 = "images/dice" + player2 + ".png"; 
 
-if (player2 > player1){
-    var text = "Player 2 Wins! 🚩";
-}else if (player1 > player2){
-     var text = "🚩Player 1 Wins!";
-}else {
-    var text = "Draw!";
+    if (player2 > player1){
+        var text = "Player 2 Wins! 🥳";
+    }else if (player1 > player2){
+        var text = "🥳Player 1 Wins!";
+    }else {
+        var text = "Draw! 😐";
+    }
+
+    // document.querySelector("#main-text h1").textContent = text;
+    document.querySelector("#player1-image").src = image1;
+    document.querySelector("#player2-image").src = image2;
+
+    if (text === "Draw!"){
+        document.querySelector("#main-text h1").innerHTML = text;
+    }
+    else{
+        document.querySelector("#main-text h1").textContent = text;
+    }
+
+    // Deskop Mode
+
+    var viewMode = getCookie("view-mode");
+    if(viewMode == "desktop"){
+        viewport.setAttribute('content', 'width=1024');
+    }else if (viewMode == "mobile"){
+        viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
+    }
+
+    return image1, image2
 }
 
-// document.querySelector("#main-text h1").textContent = text;
-document.querySelector("#player1-image").src = image1;
-document.querySelector("#player2-image").src = image2;
-
-if (text === "Draw!"){
-    document.querySelector("#main-text h1").innerHTML = text;
-}
-else{
-    document.querySelector("#main-text h1").textContent = text;
-}
-
-// Deskop Mode
-
-var viewMode = getCookie("view-mode");
-if(viewMode == "desktop"){
-    viewport.setAttribute('content', 'width=1024');
-}else if (viewMode == "mobile"){
-    viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
-}
+document.querySelectorAll(".dices")[0].addEventListener("click", function(){alert("Can't touch this.")} );
+document.querySelectorAll(".dices")[1].addEventListener("click", function(){alert("Can't touch this too.")} );
